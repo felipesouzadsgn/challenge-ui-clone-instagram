2a0fc4dba54d73adb60c41cf56d2060e47076fcc
Felipe Souza <felipesouzadsgn@gmail.com>
2022-12-14 19:27:38 -0300

    Updating code routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom"
 import { Direct } from "../pages/Direct"
 import { Explore } from "../pages/Explore"
 import { Home } from "../pages/Home"
-import { User } from "../pages/User"
+import { Profile } from "../pages/Profile"
 
 export const Routers = () => {
   const location = useLocation()
@@ -13,7 +13,7 @@ export const Routers = () => {
         <Route path="/" element={<Home />} />
         <Route path="/direct" element={<Direct />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/user" element={<User />} />
+        <Route path="/user" element={<Profile />} />
         <Route path="/p/:id"/>
       </Routes>
       {background && (
