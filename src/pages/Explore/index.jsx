799f7d97da3e144ee1ec8c69f9c36ff809472f77
Felipe Souza <felipesouzadsgn@gmail.com>
2022-12-14 21:47:43 -0300

    Updating styles on the Explore page

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -2,6 +2,12 @@ import "./_explore.scss"
 
 export const Explore = () => {
   return (
-    <h2>Explore</h2>
+    <main className="main">
+      <section className="explore">
+        <div className="explore__container">
+          {/* <h1>Explore</h1> */}
+        </div>
+      </section>
+    </main>
   )
 }
