0c996d1a24961d1ecbdda2d0b30265864d6af8c6
Felipe Souza <felipesouzadsgn@gmail.com>
2022-12-14 21:45:55 -0300

    Adding styling to the Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,79 @@
 import "./_navbar.scss"
 
+import logoInstagram from "../../assets/svg/logo-instagram.svg"
+import iconInstagram from "../../assets/svg/icon-instagram.svg"
+import iconHome from "../../assets/svg/icon-home.svg"
+import iconSearch from "../../assets/svg/icon-search.svg"
+import iconExplore from "../../assets/svg/icon-explore.svg"
+import iconMenu from "../../assets/svg/icon-menu.svg"
+import iconMensages from "../../assets/svg/icon-mensages.svg"
+import iconNotifications from "../../assets/svg/icon-notifications.svg"
+import iconPlus from "../../assets/svg/icon-plus.svg"
+import imgProfile from "../../assets/img/profile.jpg"
+
 export const Navbar = () => {
   return (
-    <h2>Navbar</h2>
+    <nav className="nav">
+      <div className="nav__menu">
+        <a className="nav__logo" href="#">
+          <img src={logoInstagram} alt="home" />
+        </a>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconHome} alt="home" />
+              Página Inicial
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconSearch} alt="home" />
+              Pesquisa
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconExplore} alt="home" />
+              Explorar
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconMensages} alt="home" />
+              Mensagens
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconNotifications} alt="home" />
+              Notificações
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__icon" src={iconPlus} alt="home" />
+              Criar
+            </a>
+          </li>
+
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              <img className="nav__img" src={imgProfile} alt="home" />
+              Perfil
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="nav__item">
+        <a className="nav__link" href="#">
+          <img className="nav__icon" src={iconMenu} alt="home" />
+          Mais
+        </a>
+      </div>
+    </nav>
   )
 }
